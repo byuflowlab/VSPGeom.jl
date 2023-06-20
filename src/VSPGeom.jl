b373ec6866ca5a2b5f2ad8f6fedbc78f0fc61d62
cibinjoseph <cibinjoseph92@gmail.com>
2023-06-20 02:02:47 -0600

    Add native STL reader and update data structures

--- src/VSPGeom.jl
+++ src/VSPGeom.jl
@@ -8,10 +8,10 @@ OpenVSP Degenerate Geometry import tool
 * License       : MIT License
 =#
 module VSPGeom
-export VSPComponent, readDegenGeom, degenGeomSize, readSTL
+export VSPComponent, readDegenGeom, degenGeomSize
+export TriMesh, readSTL, getVertices
 
 using CSV, DataFrames
-using FileIO: load, File, Stream, DataFormat
 
 """
     VSPComponent()
@@ -32,9 +32,9 @@ Parameters defining the VSPComponent object.
 - `stick_face::DataFrame`: Stick face DegenGeom
 - `point::DataFrame`: Point DegenGeom
 """
-mutable struct VSPComponent
-    type::String
+struct VSPComponent
     name::String
+    type::String
     SurfNdx::Int
     GeomID::String
     MainSurfNdx::Int
@@ -47,21 +47,30 @@ mutable struct VSPComponent
     point::DataFrame
 end
 
-# Convenience constructor
-VSPComponent(;type="", name="", SurfNdx=0, GeomID="", MainSurfNdx=0, SymCopyNdx=0,
+struct TriMesh
+    name::String
+    ncells::Int
+    normals::Vector{Vector{Float64}}
+    points::Vector{Vector{Float64}}
+    cells::Vector{Vector{Int}}
+end
+
+# Convenience constructors
+VSPComponent(name; type="", SurfNdx=0, GeomID="", MainSurfNdx=0, SymCopyNdx=0,
              surface_node=DataFrame(), surface_face=DataFrame(), plate=DataFrame(), 
              stick_node=DataFrame(), stick_face=DataFrame(), point=DataFrame()) =
-VSPComponent(type, name, SurfNdx, GeomID, MainSurfNdx, SymCopyNdx,
+VSPComponent(name, type, SurfNdx, GeomID, MainSurfNdx, SymCopyNdx,
              surface_node, surface_face, plate, stick_node, stick_face, point)
 
-function _addParams!(comp::VSPComponent, line::String)
-    strList = split(line, ",")
-    comp.type = strList[1]
-    comp.name = strList[2]
-    comp.SurfNdx = parse(Int, strList[3])
-    comp.GeomID = strList[4]
-    comp.MainSurfNdx = parse(Int, strList[5])
-    comp.SymCopyNdx = parse(Int, strList[6])
+TriMesh(name, ncells, normals; points=[], cells=[]) =
+TriMesh(name, ncells, normals, points, cells)
+
+function _addParams!(comp::VSPComponent, words)
+    comp.type = String(words[1])
+    comp.SurfNdx = parse(Int, words[3])
+    comp.GeomID = String(words[4])
+    comp.MainSurfNdx = parse(Int, words[5])
+    comp.SymCopyNdx = parse(Int, words[6])
 end
 
 function _getCSV(lines, istart::Int, nx::Int, ny::Int)
@@ -73,6 +82,89 @@ function _getCSV(lines, istart::Int, nx::Int, ny::Int)
     metadata!(csvData, "nx", nx)
     metadata!(csvData, "ny", ny)
     return csvData
+end
+
+function _isinside(p, points, tol)
+    retVal = false
+    for pt in points
+        if isapprox(p, pt; atol=tol)
+            retVal = true
+            break
+        end
+    end
+    return retVal
+end
+
+function _getSTLdata(lines)
+    name = ""
+    vertices = []
+    normals = []
+
+    # Read each line of the file
+    for line in lines
+        # Trim leading and trailing whitespaces and split to words
+        words = split(strip(line))
+
+        if isempty(words)
+            continue  # Skip empty lines
+
+        elseif words[1] == "vertex"
+            # Parse vertex coordinates
+            x = parse(Float64, words[2])
+            y = parse(Float64, words[3])
+            z = parse(Float64, words[4])
+
+            push!(vertices, [x, y, z])
+
+        elseif words[1] == "facet"
+            # Parse normal coordinates
+            x = parse(Float64, words[3])
+            y = parse(Float64, words[4])
+            z = parse(Float64, words[5])
+
+            push!(normals, [x, y, z])
+
+        elseif words[1] == "solid"
+            # Parse name of solid if present
+            if length(words) > 1
+                name = String(words[2])
+            end
+        end
+    end
+    return name, vertices, normals
+end
+
+function _getIndx(p, points; tol=eps(Float64))
+    idx = 0
+    n = length(points)
+    for i in 1:n
+        if isapprox(p, points[i], atol=tol)
+            idx = i
+            break
+        end
+    end
+    # Return 0-indexed index
+    return idx-1
+end
+
+function _dataToMesh(name::String, vertices, normals; tol::Float64=eps(Float64))
+    ncells = length(normals)
+    mesh = TriMesh(name, ncells, normals)
+    # Get list of unique points
+    for p in vertices
+        if !_isinside(p, mesh.points, tol)
+            push!(mesh.points, p)
+        end
+    end
+    # Construct cell connectivity
+    idx = zeros(Int, 3)
+    for j in 3:3:ncells*3
+        idx[1] = _getIndx(vertices[j-2], mesh.points; tol=tol)
+        idx[2] = _getIndx(vertices[j-1], mesh.points; tol=tol)
+        idx[3] = _getIndx(vertices[j  ], mesh.points; tol=tol)
+        push!(mesh.cells, deepcopy(idx))
+    end
+    return mesh
 end
 
 """
@@ -117,8 +209,9 @@ function readDegenGeom(filename::String; verbose::Bool=false)
             # start of a component
             ic += 1
             if verbose; println("Found component $ic ..."); end
-            comp[ic] = VSPComponent()
-            _addParams!(comp[ic], lines[i+1])
+            words = split(lines[i+1], ",")
+            comp[ic] = VSPComponent(String(words[2]))
+            _addParams!(comp[ic], words)
         end
 
         if occursin("SURFACE_NODE", line)
@@ -174,44 +267,59 @@ function readDegenGeom(filename::String; verbose::Bool=false)
 end
 
 """
-    readSTL(filename::String; verbose::Bool=false, binaryFormat::Bool=false)
+    readSTL(filename::String; verbose::Bool=false, tol::Float64=eps(Float64))
 
 Read STL file to obtain geometry. This function can also handle the non-standard Tagged Multi Solid file type that OpenVSP writes out. Connectivity information for the mesh is not available at present.
 
 **Arguments**
 - `filename::String`: STL filename
 - `verbose::Bool`: Set to `true` to print status messages during file read operation
-- `binaryFormat::Bool`: Set to `true` if the STL file is of binary format
+- `tol::Float64`: Set absolute tolerance when comparing points to obtain connectivity
 
 **Returns**
 - `geom`: Vector of `GeometryBasics.Mesh` objects from the ['Geometrybasics.jl'](https://juliageometry.github.io/GeometryBasics.jl/stable/) package
 """
-function readSTL(filename::String; verbose::Bool=false, binaryFormat::Bool=false)
+function readSTL(filename::String; verbose::Bool=false, tol::Float64=eps(Float64))
     geom = []
-    if binaryFormat
-        mesh = load(File{DataFormat{:STL_BINARY}}(filename))
-        if verbose; println("Found geometry 1"); end
-        push!(geom, mesh)
-    else
-        lines = readlines(filename)
-        solidEnd = Vector{Int}(undef, 0)
+    lines = readlines(filename)
+    solidEnd = Vector{Int}(undef, 0)
 
-        # Search for instances of "endsolid" inside the file to obtain geometry
-        ig = 0
-        istart = 1
-        for (i, line) in enumerate(lines)
-            if occursin("endsolid", line)
-                data = join(lines[istart:i], "\n")
-                mesh = load(Stream{DataFormat{:STL_ASCII}}(IOBuffer(data)))
-                push!(geom, mesh)
+    # Search for instances of "endsolid" inside the file to obtain geometry
+    ig = 0
+    istart = 1
+    for (i, line) in enumerate(lines)
+        if occursin("endsolid", line)
+            name, vertices, normals = _getSTLdata(lines[istart:i])
+            mesh = _dataToMesh(name, vertices, normals; tol=tol)
+            push!(geom, mesh)
 
-                ig += 1
-                if verbose; println("Found geometry $ig"); end
+            ig += 1
+            if verbose; println("Found geometry $ig"); end
 
-                istart = i + 1
-            end
+            istart = i + 1
         end
-        return geom
     end
+    return geom
+end
+
+"""
+    getVertices(mesh::TriMesh, icell::Int)
+
+Obtain vertices of a cell from a mesh
+
+**Arguments**
+- `mesh::TriMesh`: [`TriMesh`](@ref) object
+- `icell::Int`: Index of cell
+
+**Returns**
+- `vtxs`: Vector of 3 vertices of the icell cell
+"""
+function getVertices(mesh::TriMesh, icell::Int)
+    vtxs = [Vector{Float64}(undef, 3) for _ in 1:3]
+    idxs = mesh.cells[icell]
+    for i in 1:3
+        vtxs[i] = mesh.points[idxs[i]+1]
+    end
+    return vtxs
 end
 end
